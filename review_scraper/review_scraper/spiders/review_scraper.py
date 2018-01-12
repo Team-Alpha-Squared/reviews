f36be66d478332c7be396ad09763b5841044691f
nosrac77 <carson.newton@outlook.com>
2018-01-11 20:45:41 -0800

    fine tuning has proven challenging, committing changes to the review_scraper function. tried adding multiple urls.

--- review_scraper/review_scraper/spiders/review_scraper.py
+++ review_scraper/review_scraper/spiders/review_scraper.py
@@ -6,18 +6,21 @@ class ReviewSpider(scrapy.Spider):
 
     def start_requests(self):
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
-        urls = ['https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=1&type=user&sort=']
+        urls = [
+                'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=2&type=user&sort=',
+                'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=1&type=user&sort=',
+                ]
         # for i in range(52):
-        #     urls.append('https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=%s&type=user&sort=' % (i))
-        # print(urls)
+        #     url = 'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=' + str(i) + '&type=user&sort='
+        #     urls.append(url)
 
-        for url in self.start_urls:
-            yield scrapy.Request(url, headers=headers)
+        # for url in self.start_urls:
+            # yield scrapy.Request(url, headers=headers)
         for url in urls:
             yield scrapy.Request(url=url, headers=headers, callback=self.parse)
 
     def parse(self, response):
         filename = 'rt_reviews.csv'
         with open(filename, 'wb') as f:
-            f.write(str(response.css('div.review_table_row').extract()))
+            f.writelines(str(response.css('div.review_table_row').extract()))
         self.log('Saved file %s' % filename)
