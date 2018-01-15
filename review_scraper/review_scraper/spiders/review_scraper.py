eda453739087441aaece9538d7fa5bdff2fdc280
nosrac77 <carson.newton@outlook.com>
2018-01-15 14:28:37 -0800

    web scraper now gathers all reviews and writes them to individual csv files. bug involved a lack of unique names for the csv files, which require unique names for every page being scraped.

--- review_scraper/review_scraper/spiders/review_scraper.py
+++ review_scraper/review_scraper/spiders/review_scraper.py
@@ -5,22 +5,21 @@ class ReviewSpider(scrapy.Spider):
     name = "reviews"
 
     def start_requests(self):
-        headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
-        urls = [
-                'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=2&type=user&sort=',
-                'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=1&type=user&sort=',
-                ]
-        # for i in range(52):
-        #     url = 'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=' + str(i) + '&type=user&sort='
-        #     urls.append(url)
+        # headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
+        urls = []
+        for i in range(1, 52):
+            url = 'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=' + str(i) + '&type=user&sort='
+            urls.append(url)
 
         # for url in self.start_urls:
-            # yield scrapy.Request(url, headers=headers)
+            # yield scrapy.Request(url, headers=headers, callback=self.parse)
         for url in urls:
-            yield scrapy.Request(url=url, headers=headers, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        filename = 'rt_reviews.csv'
+        page = response.url.split('=')[1][0:2]
+        filename = 'rt_reviews_%s.csv' % page
         with open(filename, 'wb') as f:
-            f.writelines(str(response.css('div.review_table_row').extract()))
+            f.write(page)
+            f.write(str(response.css('div.review_table_row').extract()))
         self.log('Saved file %s' % filename)
