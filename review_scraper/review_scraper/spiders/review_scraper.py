e36fb311423847d6b012309a5b9f1e52153435e0
nosrac77 <carson.newton@outlook.com>
2018-01-15 14:41:23 -0800

    removed commented out code

--- review_scraper/review_scraper/spiders/review_scraper.py
+++ review_scraper/review_scraper/spiders/review_scraper.py
@@ -5,14 +5,11 @@ class ReviewSpider(scrapy.Spider):
     name = "reviews"
 
     def start_requests(self):
-        # headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0'}
         urls = []
         for i in range(1, 52):
             url = 'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=' + str(i) + '&type=user&sort='
             urls.append(url)
 
-        # for url in self.start_urls:
-            # yield scrapy.Request(url, headers=headers, callback=self.parse)
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
