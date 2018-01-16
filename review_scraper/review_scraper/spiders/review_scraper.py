5bee013b642d0ae2664ea31deae30ad34e15db63
nosrac77 <carson.newton@outlook.com>
2018-01-15 19:11:23 -0800

    fine-tuned crawler. it now writes the review, rating, username, and user link to an individual line in one big CSV file.

--- review_scraper/review_scraper/spiders/review_scraper.py
+++ review_scraper/review_scraper/spiders/review_scraper.py
@@ -2,9 +2,13 @@ import scrapy
 
 
 class ReviewSpider(scrapy.Spider):
+    """Class for instance of Scrapy Web-Crawler."""
+
     name = "reviews"
 
     def start_requests(self):
+        """Generate scraped webpage content."""
+
         urls = []
         for i in range(1, 52):
             url = 'https://www.rottentomatoes.com/m/star_wars_the_last_jedi/reviews/?page=' + str(i) + '&type=user&sort='
@@ -14,9 +18,20 @@ class ReviewSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split('=')[1][0:2]
-        filename = 'rt_reviews_%s.csv' % page
-        with open(filename, 'wb') as f:
-            f.write(page)
-            f.write(str(response.css('div.review_table_row').extract()))
-        self.log('Saved file %s' % filename)
+        """Write generated scraped content to CSV file."""
+
+        f = open('reviews.csv', 'a+')
+        for review in response.css('div.review_table_row'):
+            user_review = review.css('div.user_review')
+            star_rating = review.css('div.scoreWrapper').css('span')
+            username = review.css('span')
+            user_link = review.css('a').xpath('@href').extract()[0]
+            f.write(str(user_review.extract()[0].split('</div>')[1]) + ', ')
+            try:
+                f.write(str(float(star_rating.extract()[0].split('"')[1]) / 10) + ', ')
+            except ValueError:
+                f.write(star_rating.extract()[0].split('"')[1] + ', ')
+            f.write(username.extract()[0].split('>')[1].split('<')[0] + ', ')
+            f.write('https://www.rottentomatoes.com' + user_link)
+            f.write('\n')
+        f.close()
