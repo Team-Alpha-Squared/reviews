8b73a5287c3dbe242f1af40d603ff3ac09ebe604
Carson Newton <carson.newton@outlook.com>
2018-03-21 20:08:10 -0700

    added one new test to tests.py

--- review_renew/website/tests.py
+++ review_renew/website/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+"""Tests for Review Renew web application."""
 
-# Create your tests here.
+
+from django.test import TestCase
+from django.test import Client
+
+
+class TestHomeView(TestCase):
+    """Class containing home view tests."""
+
+    def setUp(self):
+        """Set up for client creation."""
+        self.client = Client()
+
+    def test_home_view_get_req_return_200_ok_res(self):
+        """Test that a GET request to the home view returns a 200 OK HTTP
+        response."""
+
+        response = self.client.get('/')
+
+        self.assertEqual(response.status_code, 200)
