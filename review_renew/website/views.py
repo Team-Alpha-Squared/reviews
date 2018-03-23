24cd8831a7685a7f3e29b6352f8bc3e8d9c164e4
Carson Newton <carson.newton@outlook.com>
2018-03-23 16:09:58 -0700

    placed load static tags in generic.html

--- review_renew/website/views.py
+++ review_renew/website/views.py
@@ -5,3 +5,9 @@ def home_view(request):
     """Function that renders Home Page of Review Renew Website."""
 
     return render(request, 'website/index.html')
+
+
+def about_view(request):
+    """Function that renders About Page of Review Renew Website."""
+
+    return render(request, 'website/generic.html')
