6312a21a280187c6ee626bd7b798c254e133e93e
Carson Newton <carson.newton@outlook.com>
2018-04-13 17:43:45 -0700

    removed about_view function from views.py as it is no longer necessary.

--- review_renew/website/views.py
+++ review_renew/website/views.py
@@ -5,9 +5,3 @@ def home_view(request):
     """Function that renders Home Page of Review Renew Website."""
 
     return render(request, 'website/index.html')
-
-
-def about_view(request):
-    """Function that renders About Page of Review Renew Website."""
-
-    return render(request, 'website/about.html')
